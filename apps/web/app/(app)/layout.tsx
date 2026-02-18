850b288b41496883a3a7b82af8a4216185cff535
Arko B <Slavehub1@>
2026-02-17 22:28:39 -0800

    Implementing client side auth protection (Clerk)

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { type ReactNode } from "react";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 import { SidebarProvider } from "@/components/layout/sidebar-provider";
 import { ThreadsProvider } from "@/hooks/use-threads";
 import { AppShell } from "@/components/layout/app-shell";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider>
-      <ThreadsProvider>
-        <AppShell>{children}</AppShell>
-      </ThreadsProvider>
-    </SidebarProvider>
+    <>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+      <SignedIn>
+        <SidebarProvider>
+          <ThreadsProvider>
+            <AppShell>{children}</AppShell>
+          </ThreadsProvider>
+        </SidebarProvider>
+      </SignedIn>
+    </>
   );
 }
